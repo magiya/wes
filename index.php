6900c397601a4ee24854b0efae60418de8e3ba04
Wesley Nyandika <nyandika.wesley@students.jkuat.ac.ke>
2016-06-20 19:00:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<title>Class Work</title>
+<style>
+body.wrap{
+  margine:0 auto;
+  width: 100%;
+  height: 100%;
+  background-color: rgba (10, 10, 10, 0.8);
+}
+</style>
+<body class="wrap" id="bodyid">
+  <h1>Welcome to Cloud computing</h1>
+  <p>This lesson is about cloud computing and the infrastructure of cloud computing.</p>
+  <p>The Infrastructure iclude:</p>
+  <ul>
+    <li>SAAS</li>
+    <li>PAAS</li>
+    <li>IAAS</li>
+  </ul>
+  <P><strong>Hope You Enjoy the Training</strong></p>
+    <p>For any information get back to as</p>
+    <form name="contact"method="post"action="">
+      E-mail:<br>
+      <input type="text"name="email" value="E-mail"/><br><br>
+      Name:<br>
+      <input type="text"name="name"value="Nama"/><br><br>
+      Message:<br>
+      <input type="textarea"name="message"value="Message"/><br><br>
+      <input type="button"name="submit" value="submit"/>
+</body>
+</html>
